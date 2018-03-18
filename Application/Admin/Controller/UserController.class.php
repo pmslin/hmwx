63d516d5a11cc2a5466e788368484d76a411dd0a
pmslin <344804202@qq.com>
2018-03-18 22:36:03 +0800

    push

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Admin\Model\UserModel;
-use Think\Controller;
+
 class UserController extends BaseController {
 
     //用户列表页面
@@ -12,18 +12,80 @@ class UserController extends BaseController {
 
     //获取用户列表
     public function getUserList(){
-        $list = D("user")->getUser();
+        $list = D("user")->getUserAndWC();
+        foreach ($list as $k=>$v){
+            $list[$k]['key']=$k+1;
+            $list[$k]['ac']='<button class="layui-btn" onclick="save('.$v['id'].')" >修改</button> 
+                <button class="layui-btn" onclick="dele('.$v['id'].')" >删除</button> ';
+        }
 
-        echo 123;
-        show_bug($list);exit();
         $this->ajaxReturn($list,'json');
     }
+
+
+    /***
+     * 账号修改
+     */
+    public function userSaveIndex(){
+        admin_check();
+
+        if (IS_POST){ //修改账号
+            $post=array(
+                "id"       =>  I("id",0),
+                "username"  =>  I("username","","trim"),
+                "tel"       =>  I("tel",0),
+                "password"  =>  I("password","","trim"),
+                "wechatAccount_id"  =>  I("wechatAccount_id",0)
+            );
+
+            $userModel=M('user');
+            $checkTel=$userModel->where(" tel=%s AND id!=%d",$post['tel'],$post['id'])->find();
+
+            if (!empty($checkTel)){
+                $msg['status']=99;
+                $msg['msg']="该手机号码已被注册！";
+            }else{
+                if (empty($post['password'])){
+                    unset($post['password']);
+                }else{
+                    $post['password'] = md5($post['password']);
+                }
+                $post['u_wc_id'] = $post['wechatAccount_id'];
+
+                $save=$userModel->where("id=%d",$post['id'])->save($post);
+                if ($save !== false){
+                    $msg['status']=1;
+                    $msg['msg']="添加修改成功";
+                }
+            }
+
+            $this->ajaxReturn($msg);
+
+        }else{ //账号修改页面
+            $user_id = I("user_id");
+            if ($user_id <= 0) $this->error("参数有误");
+
+            $userInfo = D("User")->getUserById($user_id);
+            if (empty($userInfo)) $this->error("没有这个用户");
+
+            $wcModel=D('wechat_account');
+            $wechatAccount=$wcModel->getWechatAccoun();
+            $this->assign('wechatAccount',$wechatAccount);
+
+            $this->assign("userInfo",$userInfo);
+
+            $this->display();
+        }
+    }
+
+
 
 
     /***
      * 生成账号
      */
     public function AddUser(){
+        admin_check();
 
         if ($_POST){
             $post=array(
@@ -56,6 +118,20 @@ class UserController extends BaseController {
             $this->assign('wechatAccount',$wechatAccount);
             $this->display();
         }
+    }
+
+
+    /***
+     * 删除账号
+     */
+    public function deleteUser(){
+        admin_check();
+
+        $user_id = I("id",0);
+        if ($user_id <= 0) $this->error("参数有误");
+
+        $delete = D("User")->deteleById($user_id);
+        $delete ? $this->success("删除成功") : $this->error("删除失败");
     }
 
 
