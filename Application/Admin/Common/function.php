dc47ca893c2b5c9a8f123cc3c78dce2f1a991588
pmslin <344804202@qq.com>
2018-03-20 22:25:41 +0800

    push

--- Application/Admin/Common/function.php
+++ Application/Admin/Common/function.php
@@ -1,49 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Lin
- * Date: 2017/5/1
- * Time: 10:35
- */
+
 
 /**
- * 教务+图书管理员权限
- *  发书权限检测，roleid=1和4的可以修改，1超管 4教务 6图书管理
+ * 超管
+ *  账号管理员权限检测，roleid=1和2的可以修改，1超管 2账号管理员
  */
-function book_check()
+function admin_check()
 {
-    if (!in_array(session('roleid'), array(1, 4, 6))) {
+    if (!in_array(session('roleid'), array(1, 2))) {
 //         error('没有操作权限');
         echo '没有操作权限';
         exit();
     }
 }
 
-/**
- * 教务权限
- *  发书权限检测，roleid=1和4的可以修改，1超管 4教务
- */
-function teacher_check()
-{
-    if (!in_array(session('roleid'), array(1, 4))) {
-//         error('没有操作权限');
-        echo '没有操作权限';
-        exit();
-    }
-}
 
-/**
- * 财务权限
- *  财务权限检测，roleid=1和4的可以修改，1超管 5财务
- */
-function cost_check()
-{
-    if (!in_array(session('roleid'), array(1, 5))) {
-//         error('没有操作权限');
-        echo '没有操作权限';
-        exit();
-    }
-}
+
+
+
+
 
 
 /**
@@ -197,164 +172,3 @@ function exportExcel($data, $title, $subject, $ary = null)
     $objWriter->save('php://output');
 }
 
-// $datasec = array ();
-//$ctrl_dir = array ();
-// $eof_ctrl_dir = "\x50\x4b\x05\x06\x00\x00\x00\x00";
-// $old_offset = 0;
-////导出图片
-//function downImg()
-//{
-//    //下面是实例操作过程：
-//    $dfile = tempnam('/tmp', 'tmp');//产生一个临时文件，用于缓存下载文件
-////        $zip = new zipfile();
-////----------------------
-//    $filename = 'image.zip'; //下载的默认文件名
-//
-////以下是需要下载的图片数组信息，将需要下载的图片信息转化为类似即可
-//    $image = array(
-//        array('image_src' => 'public/Uploads/2017-05-25/5926d4f91d17d.jpg', 'image_name' => '图片1.jpg'),
-//        array('image_src' => 'public/Uploads/2017-05-25/5926dcc33a193.jpg', 'image_name' => 'pic/图片2.jpg'),
-//    );
-//
-//    foreach ($image as $v) {
-//        add_file(file_get_contents($v['image_src']), $v['image_name']);
-//        // 添加打包的图片，第一个参数是图片内容，第二个参数是压缩包里面的显示的名称, 可包含路径
-//        // 或是想打包整个目录 用 $zip->add_path($image_path);
-//    }
-////----------------------
-//    output($dfile);
-//
-//// 下载文件
-//    ob_clean();
-//    header('Pragma: public');
-//    header('Last-Modified:' . gmdate('D, d M Y H:i:s') . 'GMT');
-//    header('Cache-Control:no-store, no-cache, must-revalidate');
-//    header('Cache-Control:pre-check=0, post-check=0, max-age=0');
-//    header('Content-Transfer-Encoding:binary');
-//    header('Content-Encoding:none');
-//    header('Content-type:multipart/form-data');
-//    header('Content-Disposition:attachment; filename="' . $filename . '"'); //设置下载的默认文件名
-//    header('Content-length:' . filesize($dfile));
-//    $fp = fopen($dfile, 'r');
-//    while (connection_status() == 0 && $buf = @fread($fp, 8192)) {
-//        echo $buf;
-//    }
-//    fclose($fp);
-//    @unlink($dfile);
-//    @flush();
-//    @ob_flush();
-//    exit();
-//}
-//
-//
-//function unix2_dostime($unixtime = 0)
-//{
-//    $timearray = ($unixtime == 0) ? getdate() : getdate($unixtime);
-//    if ($timearray ['year'] < 1980) {
-//        $timearray ['year'] = 1980;
-//        $timearray ['mon'] = 1;
-//        $timearray ['mday'] = 1;
-//        $timearray ['hours'] = 0;
-//        $timearray ['minutes'] = 0;
-//        $timearray ['seconds'] = 0;
-//    }
-//    return (($timearray ['year'] - 1980) << 25) | ($timearray ['mon'] << 21) | ($timearray ['mday'] << 16) | ($timearray ['hours'] << 11) | ($timearray ['minutes'] << 5) | ($timearray ['seconds'] >> 1);
-//}
-//
-//function add_file($data, $name, $time = 0)
-//{
-//    $name = str_replace('\\', '/', $name);
-//
-//    $dtime = dechex(unix2_dostime($time));
-//    $hexdtime = '\x' . $dtime [6] . $dtime [7] . '\x' . $dtime [4] . $dtime [5] . '\x' . $dtime [2] . $dtime [3] . '\x' . $dtime [0] . $dtime [1];
-//    eval('$hexdtime = "' . $hexdtime . '";');
-//
-//    $fr = "\x50\x4b\x03\x04";
-//    $fr .= "\x14\x00";
-//    $fr .= "\x00\x00";
-//    $fr .= "\x08\x00";
-//    $fr .= $hexdtime;
-//
-//    $unc_len = strlen($data);
-//    $crc = crc32($data);
-//    $zdata = gzcompress($data);
-//    $zdata = substr(substr($zdata, 0, strlen($zdata) - 4), 2);
-//    $c_len = strlen($zdata);
-//    $fr .= pack('V', $crc);
-//    $fr .= pack('V', $c_len);
-//    $fr .= pack('V', $unc_len);
-//    $fr .= pack('v', strlen($name));
-//    $fr .= pack('v', 0);
-//    $fr .= $name;
-//
-//    $fr .= $zdata;
-//    $fr .= pack('V', $crc);
-//    $fr .= pack('V', $c_len);
-//    $fr .= pack('V', $unc_len);
-//
-//    $datasec [] = $fr;
-//
-//    $cdrec = "\x50\x4b\x01\x02";
-//    $cdrec .= "\x00\x00";
-//    $cdrec .= "\x14\x00";
-//    $cdrec .= "\x00\x00";
-//    $cdrec .= "\x08\x00";
-//    $cdrec .= $hexdtime;
-//    $cdrec .= pack('V', $crc);
-//    $cdrec .= pack('V', $c_len);
-//    $cdrec .= pack('V', $unc_len);
-//    $cdrec .= pack('v', strlen($name));
-//    $cdrec .= pack('v', 0);
-//    $cdrec .= pack('v', 0);
-//    $cdrec .= pack('v', 0);
-//    $cdrec .= pack('v', 0);
-//    $cdrec .= pack('V', 32);
-//
-//    $cdrec .= pack('V', old_offset);
-//    $old_offset += strlen($fr);
-//
-//    $cdrec .= $name;
-//
-//    $ctrl_dir[] = $cdrec;
-//}
-//
-//function add_path($path, $l = 0)
-//{
-//    $d = @opendir($path);
-//    $l = $l > 0 ? $l : strlen($path) + 1;
-//    while ($v = @readdir($d)) {
-//        if ($v == '.' || $v == '..') {
-//            continue;
-//        }
-//        $v = $path . '/' . $v;
-//        if (is_dir($v)) {
-//            add_path($v, $l);
-//        } else {
-//            add_file(file_get_contents($v), substr($v, $l));
-//        }
-//    }
-//}
-//
-//function files()
-//{
-//    $data = implode('',  datasec);
-//    $ctrldir = implode('',  ctrl_dir);
-//    return $data . $ctrldir .  eof_ctrl_dir . pack('v', sizeof( ctrl_dir)) . pack('v', sizeof( ctrl_dir)) . pack('V', strlen($ctrldir)) . pack('V', strlen($data)) . "\x00\x00";
-//}
-//
-//function add_files($files)
-//{
-//    foreach ($files as $file) {
-//        if (is_file($file)) {
-//            $data = implode("", file($file));
-//            add_file($data, $file);
-//        }
-//    }
-//}
-//
-//function output($file)
-//{
-//    $fp = fopen($file, "w");
-//    fwrite($fp, files());
-//    fclose($fp);
-//}
